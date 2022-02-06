bde006fbb2d01af9bf1b5b6b0178f795d4bddef2
subeen-me <subeen.me@gmail.com>
2022-02-06 19:51:38 +0900

    arrcnt

---
+++ src/Algorithm/Array/등수구하기.java
@@ -0,0 +1,29 @@
+package Algorithm.Array;
+
+import java.util.Scanner;
+
+public class 등수구하기 {
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+        for(int i=0; i<n; i++) {
+            int cnt = 1;
+            for(int j = 0; j<n; j++) {
+                if(arr[j]>arr[i]) cnt++;
+            }
+            answer[i] = cnt;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        등수구하기 T = new 등수구하기();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for(int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
+}
