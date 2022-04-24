9dec229c2790250f03e5527dc6ea9f3e07b80144
subeen-me <subeen.me@gmail.com>
2022-04-25 02:19:37 +0900

    binarySearch메서드 검색

---
+++ src/DoitAlgorithm/chap03/BinarySearchTester.java
@@ -0,0 +1,36 @@
+package DoitAlgorithm.chap03;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinarySearchTester {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("요솟수 : ");
+        int num = kb.nextInt();
+        int[] x = new int[num];
+
+        System.out.println("오름차순으로 입력하세요.");
+
+        System.out.print("x[0] : ");
+        x[0] = kb.nextInt();
+
+        for(int i=1; i<num; i++) {
+            do {
+                System.out.print("x["+i+"]: ");
+                x[i] = kb.nextInt();
+            } while (x[i] < x[i-1]);
+        }
+
+        System.out.print("검색할 값 :");
+        int ky = kb.nextInt();
+
+        int idx = Arrays.binarySearch(x, ky); //배열 x에서 키 값이 ky인 요소를 검색
+
+        if(idx == -1)
+            System.out.println("그 값의 요소가 없습니다.");
+        else
+            System.out.println(ky+"은/는 x["+idx+"]에 있습니다.");
+    }
+}
