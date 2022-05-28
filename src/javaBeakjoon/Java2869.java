515d06414c837b0dc4e5723033c9ec5440ea7ff9
subeen-me <subeen.me@gmail.com>
2022-05-28 23:00:58 +0900

    2869번

--- src/javaBeakjoon/Java2869.java
+++ src/javaBeakjoon/Java2869.java
@@ -1,4 +1,23 @@
 package javaBeakjoon;
 
+import java.util.Scanner;
+
 public class Java2869 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int up = kb.nextInt();
+        int down = kb.nextInt();
+        int length = kb.nextInt();
+        int day = (length - down) / (up - down);
+
+        //나머지가 있을 경우
+        if((length - down) % ( up - down) != 0) {
+            day++;
+        }
+
+        System.out.println(day);
+
+    }
+
+
 }
