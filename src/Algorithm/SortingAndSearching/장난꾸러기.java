27f80a7c34b4e346471de2488e7a8b1acee9aa38
subeen-me <subeen.me@gmail.com>
2022-07-18 23:51:27 +0900

    정렬

---
+++ src/Algorithm/SortingAndSearching/장난꾸러기.java
@@ -0,0 +1,25 @@
+package Algorithm.SortingAndSearching;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 장난꾸러기 {
+    public ArrayList<Integer> solution(int n, int[] arr){
+        ArrayList<Integer> answer=new ArrayList<>();
+        int[] tmp=arr.clone();
+        Arrays.sort(tmp);
+        for(int i=0; i<n; i++){
+            if(arr[i]!=tmp[i]) answer.add(i+1);
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        장난꾸러기 T = new 장난꾸러기();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        for(int x : T.solution(n, arr)) System.out.print(x+" ");
+    }
+}
