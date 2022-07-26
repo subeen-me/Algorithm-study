e5dc5d24c022189d0c1115d3a986f2a81675105e
subeen-me <subeen.me@gmail.com>
2022-07-26 23:03:48 +0900

    투포인터

---
+++ src/Algorithm/Twopointers/자연수의합2.java
@@ -0,0 +1,29 @@
+package Algorithm.Twopointers;
+
+import java.util.Scanner;
+
+public class 자연수의합2 {
+    public int solution(int n){
+        int answer=0, sum=0;
+        int m=n/2+1;
+        int[] arr=new int[m];
+        for(int i=0; i<m; i++) arr[i]=i+1;
+        int lt=0;
+        for(int rt=0; rt<m; rt++){
+            sum+=arr[rt];
+            if(sum==n) answer++;
+            while(sum>=n){
+                sum-=arr[lt++];
+                if(sum==n) answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        자연수의합2 T = new 자연수의합2();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        System.out.print(T.solution(n));
+    }
+}
