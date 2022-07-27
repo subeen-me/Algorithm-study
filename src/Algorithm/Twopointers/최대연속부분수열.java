c722f44ebbf7f82c52ae8abc3183500aff6202d4
subeen-me <subeen.me@gmail.com>
2022-07-27 23:22:10 +0900

    수학

---
+++ src/Algorithm/Twopointers/최대연속부분수열.java
@@ -0,0 +1,30 @@
+package Algorithm.Twopointers;
+
+import java.util.Scanner;
+
+public class 최대연속부분수열 {
+    public int solution(int n, int k, int[] arr){
+        int answer=0, cnt=0, lt=0;
+        for(int rt=0; rt<n; rt++){
+            if(arr[rt]==0) cnt++;
+            while(cnt>k){
+                if(arr[lt]==0) cnt--;
+                lt++;
+            }
+            answer=Math.max(answer, rt-lt+1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        최대연속부분수열 T = new 최대연속부분수열();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int k=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+        System.out.print(T.solution(n, k, arr));
+    }
+}
