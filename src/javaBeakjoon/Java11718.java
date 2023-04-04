f0ea7e1f0e9879b0d09a9f1d3327a83793d99dfd
subeen-me <subeen.me@gmail.com>
2023-04-05 00:34:05 +0900

    문자열

---
+++ src/javaBeakjoon/Java11718.java
@@ -0,0 +1,15 @@
+package javaBeakjoon;
+
+import java.util.Scanner;
+
+public class Java11718 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        while(kb.hasNext()) {
+            System.out.println(kb.nextLine());
+        }
+        kb.close();
+
+    }
+}
