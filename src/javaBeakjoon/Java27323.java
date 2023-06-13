e9675b01cfd6a7c1bcdca59fbd582e4332f88119
subeen-me <91585195+subeen-me@users.noreply.github.com>
2023-06-13 23:01:35 +0900

    수학

---
+++ src/javaBeakjoon/Java27323.java
@@ -0,0 +1,12 @@
+package javaBeakjoon;
+
+import java.util.Scanner;
+
+public class Java27323 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int a = kb.nextInt();
+        int b = kb.nextInt();
+        System.out.println(a * b);
+    }
+}
