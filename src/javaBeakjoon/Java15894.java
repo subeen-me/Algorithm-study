4982e09b968f1d3b1e3c717c58e7be72c6271c27
subeen-me <91585195+subeen-me@users.noreply.github.com>
2023-06-13 23:01:30 +0900

    수학

---
+++ src/javaBeakjoon/Java15894.java
@@ -0,0 +1,13 @@
+package javaBeakjoon;
+
+import java.util.Scanner;
+
+public class Java15894 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        long n = kb.nextLong();
+
+        System.out.println(n * 4);
+    }
+}
