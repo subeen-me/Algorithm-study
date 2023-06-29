eb61468cab9826e4eb590110192325b22a7f899a
subeen-me <91585195+subeen-me@users.noreply.github.com>
2023-06-30 02:22:56 +0900

    해시

---
+++ src/javaBeakjoon/Java7785.java
@@ -0,0 +1,35 @@
+package javaBeakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Java7785 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+
+        HashMap<String, String> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String state = st.nextToken();
+
+            if (map.containsKey(name)) {
+                map.remove(name);
+            } else {
+                map.put(name, state);
+            }
+        }
+
+        ArrayList<String> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, Collections.reverseOrder());
+
+        for(var li : list) {
+            System.out.println(li);
+        }
+    }
+}
