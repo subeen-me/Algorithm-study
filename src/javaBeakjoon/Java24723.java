0053fe566118063a46049f1b471d65e8352b0190
subeen-me <91585195+subeen-me@users.noreply.github.com>
2023-07-08 22:21:35 +0900

    수학

---
+++ src/javaBeakjoon/Java24723.java
@@ -0,0 +1,15 @@
+package javaBeakjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Java24723 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println((int) Math.pow(2, n));
+    }
+}
