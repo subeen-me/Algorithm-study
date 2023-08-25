ccff464e7d3f4f1cfa88a608bce7d219e57ac218
subeen-me <91585195+subeen-me@users.noreply.github.com>
2023-08-25 23:12:07 +0900

    수학

---
+++ src/javaBeakjoon/Java18018.java
@@ -0,0 +1,12 @@
+package javaBeakjoon;
+
+import java.util.Scanner;
+
+public class Java18018 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int y = kb.nextInt();
+        System.out.println(y - 543);
+    }
+}
