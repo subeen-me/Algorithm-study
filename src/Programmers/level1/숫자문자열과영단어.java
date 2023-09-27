48db4785dafa605255900561174d9f33d4fac951
subeen-me <91585195+subeen-me@users.noreply.github.com>
2023-09-28 01:27:48 +0900

    문자열

---
+++ src/Programmers/level1/숫자문자열과영단어.java
@@ -0,0 +1,16 @@
+package Programmers.level1;
+
+public class 숫자문자열과영단어 {
+    public int solution(String s) {
+
+        String[] num = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+        for(int i=0; i<10; i++) {
+            s = s.replace(num[i], Integer.toString(i));
+        }
+
+        int answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
