abfa799bc9570152d4530a8be95b5e8ae14a5168
subeen-me <91585195+subeen-me@users.noreply.github.com>
2023-10-31 21:31:32 +0900

    배열

---
+++ src/Programmers/level1/카드뭉치.java
@@ -0,0 +1,20 @@
+package Programmers.level1;
+
+public class 카드뭉치 {
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+        String answer = "Yes";
+
+        int idx1 = 0;
+        int idx2 = 0;
+
+        for(String word : goal) {
+            if(idx1 < cards1.length && word.equals(cards1[idx1])) {
+                idx1++;
+            } else if (idx2 < cards2.length && word.equals(cards2[idx2])) {
+                idx2++;
+            } else return "No";
+        }
+
+        return answer;
+    }
+}
